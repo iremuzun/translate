0bce1674e50ec35a791ce4fec31d3477c525be06
İrem Uzun <69756706+iremuzun@users.noreply.github.com>
2020-12-14 20:07:19 +0300

    Add files via upload

---
+++ translator.py
@@ -0,0 +1,33 @@
+from googletrans import Translator
+
+translator = Translator()
+
+
+result = translator.translate('aceleración de una partícula')
+
+'''src: The source language
+dest: Destination language
+origin: Original text, that
+text: Translated text, that will be 
+pronunciation: Pronunciation of the translated text'''
+
+
+print(result.src)
+print(result.dest)
+print(result.origin)
+print(result.text)
+print(result.pronunciation)
+
+result = translator.translate('aceleración de una partícula', src='es', dest='tr')
+ 
+
+print(result.src)
+print(result.dest)
+print(result.text)
+
+sentences = ['movimiento circular uniforme', 'aceleración centrípeta', 'velocidad angular']
+
+result = translator.translate(sentences, src='es' , dest = 'tr')
+
+for trans in result:
+    print(f'{trans.origin} -> {trans.text}')
